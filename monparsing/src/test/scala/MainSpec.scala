f2ed1166bace3a4c2ec71cc4355f829d46ed010a
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-04-18 21:21:40 -0300

    fix: typo

--- monparsing/src/test/scala/MainSpec.scala
+++ monparsing/src/test/scala/MainSpec.scala
@@ -20,7 +20,7 @@ class MainSpec extends AnyFlatSpec with should.Matchers {
     item("") should be(List())
   }
 
-  "flatMap" should "pass results to second parser and first parser succeeds" in {
+  "bind" should "pass results to second parser and first parser succeeds" in {
     bind(item)(a => result(a.toUpper))("hello") should be(List(('H', "ello")))
     bind(item)(a => result(a.toUpper))("") should be(List())
   }
